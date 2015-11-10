bec50f6d156d2c86a6d85be7e6ed5eebafaf30d4
Steve Gledhill <stevegleds@users.noreply.github.com>
2015-11-10 22:40:49 +0000

    Added timetrial distance constant

--- timetrial/timetrial.py
+++ timetrial/timetrial.py
@@ -2,14 +2,18 @@
 # testing
 # import matplotlib.pyplot as plt
 # import numpy as np
-# import csv
+import csv
 import tkinter as tk
 from collections import Counter
+import os as system
+
+TIME_TRIAL_DISTANCE = 3.0 #TODO use this instead of distance 
+
 
 # Working through https://automatetheboringstuff.com/chapter12/#calibre_link-64
 # 10 July 2015 branched to follow newcoder.io tutorial on data visualization
 
-RUN_FILE = 'timetrial.csv'
+RUN_FILE = 'timetrial/timetrial.csv'
 
 
 def parse(raw_file, delimiter):
@@ -35,7 +39,7 @@ def parse(raw_file, delimiter):
         parsed_data.append(dict(zip(fields, row)))  # Creates a new dict item for each row with col header as key
         #  racer_count += 1  # This is number of racers not races
     # count number of races
-    race_counter = Counter(item['Race'] for item in parsed_data)  # stores the number of races
+    race_counter = Counter(item['Date'] for item in parsed_data)  # stores the number of races
     print("number of races is: ", len(race_counter))
     print("the races are: ")
     for k, v in race_counter.items():
@@ -54,7 +58,7 @@ def visualize_races(data_file):
     #  use 'counter' to count the races
     dates = []
     # counter = Counter(item['Date'] for item in data_file) #  list comprehension: pythonic loop construction
-    runner_counter = Counter(item['Name'] for item in data_file)  # stores the number of races per runner
+    runner_counter = Counter(item['Runner'] for item in data_file)  # stores the number of races per runner
     print("runner counter is: ", runner_counter)
     runner_list = [runner for runner in sorted(runner_counter)]  # stores list of runners full names
     runner_list.pop(0)  # first value is the null value
@@ -133,7 +137,7 @@ def visualize_type(new_data):
     return
 
 def get_runners_list(new_data): 
-    runner_counter = Counter(item['Name'] for item in new_data)  # stores the number of races per runner
+    runner_counter = Counter(item['Runner'] for item in new_data)  # stores the number of races per runner
     print("runner counter is: ", runner_counter)
     runners_list = [runner for runner in sorted(runner_counter)]  # stores list of runners full names
     runners_list.pop(0)  # first value is the null value
@@ -152,7 +156,7 @@ def get_distances(new_data):
     return total_distance, championship_distance
 
 def get_runners(new_data): # go through the data file to get a list of runners
-    runner_counter = Counter(item['Name'] for item in data_file)  # stores the number of races per runner
+    runner_counter = Counter(item['Runner'] for item in data_file)  # stores the number of races per runner
     print("runner counter is: ", runner_counter)
     runners_list = [runner for runner in sorted(runner_counter)]  # stores list of runners full names
     runners_list.pop(0)  # first value is the null value
@@ -166,10 +170,10 @@ def get_runners_distances(new_data, runners_list): #TODO this is not working
         runners_distances[runner] = 0 # set to 0 ready to add race distances
     print('runners distance dict is: ', runners_distances)
     for race in new_data:
-        print('name is: ', race['Name'], 'distance is : ', race['Miles'])
+        print('name is: ', race['Runner'], 'distance is : ', race['Miles'])
         try:
-            runners_distances[race['Name']] += float(race['Miles'])
-            print(race['Name'], ' has raced ', runners_distances[race['Name']], ' at the end of the ', race['Race'], 'race') # TODO need to use formats here
+            runners_distances[race['Runner']] += float(race['Miles'])
+            print(race['Runner'], ' has raced ', runners_distances[race['Runner']], ' at the end of the ', race['Race'], 'race') # TODO need to use formats here
             # Error Need to trap last race
         except:
             pass # we have reached the end of the races . TODO trap this better we are also storing null values. Look at csv file.
@@ -199,12 +203,6 @@ def main():
     print("Runners are: ", runners_list)
     runners_distances = get_runners_distances(new_data, runners_list)
     print("Runners distances are:", runners_distances)
-    #  print("There were this number of races: ", race_count)
-    #  for dict_item in new_data:
-        #  print(type(dict_item["Name"]))
-        #  print(dict_item["Date"])
-    #  visualize_races(new_data)
-    #  visualize_type(new_data)
     total_distance, championship_distance = get_distances(new_data)
     print('Total distance run : ', total_distance, 'of which ', championship_distance, 'miles were in the summer championship')
     present_race_information(total_distance, championship_distance)
